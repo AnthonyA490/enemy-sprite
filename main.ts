bf37dee20bf21ff11141e3b02ebf5e262c5c3f2f
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-19 10:54:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ player = game.createSprite(2, 2)
 basic.forever(function () {
     if (player.isTouching(enemy)) {
         basic.showIcon(IconNames.Happy)
+        enemy.delete()
         enemy = game.createSprite(randint(0, 3), randint(0, 3))
     }
 })
